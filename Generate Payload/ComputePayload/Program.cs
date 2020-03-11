141195243fcb6ea3ce5cca8cc030389925d5dbb5
davidcristian <61152966+davidcristian@users.noreply.github.com>
2020-03-11 12:41:23 +0200

    Update help text

--- Generate Payload/ComputePayload/Program.cs
+++ Generate Payload/ComputePayload/Program.cs
@@ -167,7 +167,8 @@ namespace ComputePayload
         /// </summary>
         private static void PrintHelp()
         {
-            Console.WriteLine($"Usage: .\\{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name} [OPTION]... [FILE]... [-C]");
+            Console.WriteLine($"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}");
+            Console.WriteLine($@"Usage: .\{AppDomain.CurrentDomain.FriendlyName} [OPTION]... [FILE]... [-C]");
             Console.WriteLine("  -e    Compress a file using GZip and encode it into a base64 string");
             Console.WriteLine("  -f    Fix the bad characters in a file (use on one-liners)");
             Console.WriteLine("  -c    Copy the generated commands into the clipboard");
